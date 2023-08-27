bbba8036487bdbe219cf2ffc4e2516063b29740c
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-08-27 09:06:53 -0400

    just set up login component moving on to signup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
 import './App.css'
 import React from 'react'
+import Login from './components/Login'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div>
-      Linkedin Clone
-
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          
+        </Routes>
+      </Router>
+   
     </div>
+
   )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import  styled  from 'styled-components';
+
+export default function Login(props) {
+  return (
+  
+    <Container>
+        <Nav>
+            <a href="/">
+                <img src="/images/login-logo.svg" alt=""/>
+            </a>
+            <div>
+                <Join>Join Now</Join>
+            </div>
+        </Nav>
+    </Container>
+   
+  )
+}
+const Container = styled.div`
+    padding: 0px;
+
+`
+const Join = styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    border-radius: 4px;
+    color: rgba(0,0,0,0.6);
+    margin-right: 12px;
+    &:hover {
+        background-color: rgba(0,0,0,0.08);
+        color: rgba(0,0,0,0.9);
+        text-decoration: none;
+
+
+    }
+
+`
+const Nav = styled.nav`
+    max-width: 1128px;
+    margin: auto;
+    padding: 12px 0 16px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+
+    & > a{
+        width: 135px;
+        height: 34px;
+        @media (min-width: 768px){
+            padding: 0 5px;
+        }
+    }
+  
+    
+`
+
+    
